03549212dacab2daa92a31db38af4a6ff17283c6
jatisto <j.atisto@gmail.com>
2018-12-14 19:13:56 +0600

    Changeappsetting.json
    изменил на .\\SQLEXPRESS

---
+++ inventory_accounting_system/inventory_accounting_system/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace inventory_accounting_system.Models
+{
+    public class Category : Entity
+    {
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+    }
+}
