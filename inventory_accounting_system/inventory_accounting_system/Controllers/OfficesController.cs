de0c6ddf8b44b9193a60d4851745dba42cc6ec7d
Yiogurtina <s.aizhana@gmail.com>
2018-12-15 16:58:06 +0600

    Auto stash before merge of "develop" and "origin/develop"

---
+++ inventory_accounting_system/inventory_accounting_system/Controllers/OfficesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using inventory_accounting_system.Data;
+using inventory_accounting_system.Models;
+
+namespace inventory_accounting_system.Controllers
+{
+    public class OfficesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OfficesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Offices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Offices.ToListAsync());
+        }
+
+        // GET: Offices/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // GET: Offices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Offices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Id")] Office office)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(office);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(office);
+        }
+
+        // GET: Offices/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices.SingleOrDefaultAsync(m => m.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+            return View(office);
+        }
+
+        // POST: Offices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Title,Id")] Office office)
+        {
+            if (id != office.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(office);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OfficeExists(office.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(office);
+        }
+
+        // GET: Offices/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var office = await _context.Offices
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (office == null)
+            {
+                return NotFound();
+            }
+
+            return View(office);
+        }
+
+        // POST: Offices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var office = await _context.Offices.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Offices.Remove(office);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OfficeExists(string id)
+        {
+            return _context.Offices.Any(e => e.Id == id);
+        }
+    }
+}
