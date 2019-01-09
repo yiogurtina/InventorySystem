c88febb541cb30f8bd068988c2def827c8e8b17c
jatisto <j.atisto@gmail.com>
2019-01-09 09:18:50 +0600

    #32 BagFix Added a space to the validation regular expression
    Добавил символ пробела в регулярное выражение валидации.

--- inventory_accounting_system/inventory_accounting_system/Models/Supplier.cs
+++ inventory_accounting_system/inventory_accounting_system/Models/Supplier.cs
@@ -10,7 +10,7 @@ namespace inventory_accounting_system.Models
     {
         [Required(ErrorMessage = "Название Поставщика не должно быть пустым")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Название Поставщика не должно быть короче 3 символов и длиннее 20")]
-        [RegularExpression("^[а-яА-Яa-zA-Z0-9]*$", ErrorMessage = "Можно использовать только буквы и цифры")]
+        [RegularExpression("^[а-яА-Яa-zA-Z0-9 ]*$", ErrorMessage = "Можно использовать только буквы и цифры")]
         [Display(Name = "Имя")]
         public string Name { get; set; }
 
