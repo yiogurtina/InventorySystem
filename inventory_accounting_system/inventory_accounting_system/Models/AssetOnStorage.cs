726ac42dec4f865d8ae51be5db6dc46239702420
Yiogurtina <s.aizhana@gmail.com>
2019-01-11 11:26:56 +0600

    #59 Add model Asset on Storage

---
+++ inventory_accounting_system/inventory_accounting_system/Models/AssetOnStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventory_accounting_system.Models
+{
+    public class AssetOnStorage : Entity
+    {
+        public string AssetId { get; set; }
+        public Asset Asset { get; set; }
+
+        public string EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        public string StorageId { get; set; }
+        public Storage Storage { get; set; }
+    }
+}
