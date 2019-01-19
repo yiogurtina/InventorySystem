0f4a979f4593d2b96df7d0b8ef621e5804dbfc78
EugeneTM <j.atisto@gmail.com>
2019-01-20 00:50:29 +0600

    #86 Number of unread tickets (Admin role)

---
+++ inventory_accounting_system/inventory_accounting_system/ViewModel/OrderEmployeeAdminVM.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using inventory_accounting_system.Data;
+using inventory_accounting_system.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace inventory_accounting_system.ViewModel
+{
+    public class OrderEmployeeAdminVM : ViewComponent
+    {
+        #region Dependency Injection
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Employee> _userManager;
+
+        public OrderEmployeeAdminVM(ApplicationDbContext context, UserManager<Employee> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        #endregion
+
+        public IEnumerable<OrderMessageAdminViewModel> OrderMessageAdminViewModels { get; set; }
+
+        public string Invoke()
+        {
+            var orderMessage = _context.OrderEmployeeAdmins
+                .Where(s => s.StatusAdmin == "New")
+                .GroupBy(a => new { a.Id })
+                .Select(g => new OrderMessageViewModel
+                {
+                    MessageCount = g.Count()
+                }).ToList();
+
+            return orderMessage.Count.ToString();
+        }
+
+        public class OrderMessageAdminViewModel
+        {
+            public int MessageCountAdmin { get; set; }
+        }
+    }
+}
