0b35ab24c107abe1a9e6bc7194b685bb6b7df727
EugeneTM <j.atisto@gmail.com>
2019-02-09 13:46:07 +0600

    #32 BagFix Test

--- inventory_accounting_system/UnitTestProjectIAS/SuppliersTest.cs
+++ inventory_accounting_system/UnitTestProjectIAS/SuppliersTest.cs
@@ -1,136 +1,121 @@
-﻿using inventory_accounting_system.Data;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using inventory_accounting_system.Controllers;
+using inventory_accounting_system.Data;
 using inventory_accounting_system.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
 using Assert = Xunit.Assert;
 
-namespace UnitTestProjectIAS
-{
-    public class SuppliersTest
-    {
+namespace UnitTestProjectIAS {
+    public class SuppliersTest {
         [TestMethod]
         [Fact]
-        public void SuppliersCreate()
-        {
+        public void SuppliersCreate () {
 
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "aspnet-InventorySystem")
+            var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
+                .UseInMemoryDatabase (databaseName: "aspnet-InventorySystem")
                 .Options;
 
-            using (var _context = new ApplicationDbContext(options))
-            {
+            using (var _context = new ApplicationDbContext (options)) {
 
-                Supplier vendor = new Supplier() { Id = "1" };
-                var controller = new SuppliersController(_context);
-                var result1 = controller.Create(vendor);
-                var rSearchId = _context.Suppliers.SingleOrDefault(a => a.Id == "1");
-                Assert.NotNull(result1);
-                Assert.Equal(rSearchId.Id, "1");
-              
+                Supplier vendor = new Supplier () { Id = "1" };
+                var controller = new SuppliersController (_context);
+                var result1 = controller.Create (vendor);
+                var rSearchId = _context.Suppliers.SingleOrDefault (a => a.Id == "1");
+                Assert.NotNull (result1);
+                Assert.Equal (rSearchId.Id, "1");
 
             }
 
         }
 
         [Fact]
-        public void SupplierDelete()
-        {
-            
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "aspnet-InventorySystem")
+        public void SupplierDelete () {
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
+                .UseInMemoryDatabase (databaseName: "aspnet-InventorySystem")
                 .Options;
 
-            using (var _context = new ApplicationDbContext(options))
-            {
+            using (var _context = new ApplicationDbContext (options)) {
 
-                Supplier vendor = new Supplier() { Id = "1" };
-                _context.Add(vendor);
-                _context.SaveChangesAsync();
-                var controller = new SuppliersController(_context);
-                Supplier supp = new Supplier();
-                Task<IActionResult> result1 = controller.Delete(vendor.Id);
-                var supSearch = _context.Suppliers.SingleOrDefault(a => a.Id == vendor.Id);
+                Supplier vendor = new Supplier () { Id = "1"};
+                _context.Add (vendor);
+                _context.SaveChangesAsync ();
+                SuppliersController controller = new SuppliersController (_context);
+             
+                Task<IActionResult> result1 = controller.DeleteConfirmed (vendor.Id);
+                var supSearch = _context.Suppliers.SingleOrDefault (a => a.Id == vendor.Id);
 
-                Assert.NotNull(result1);
-                Assert.Equal(null, supSearch);
+                Assert.NotNull (result1);
+                Assert.Equal (supSearch, null);
             }
         }
 
         [Fact]
-        public void SuppliersEdit()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+        public void SuppliersEdit () {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
+                .UseInMemoryDatabase (Guid.NewGuid ().ToString ())
                 .Options;
 
-            using (var _context = new ApplicationDbContext(options))
-            {
-                Supplier vendor = new Supplier() { Id = "1", Name = "Samsung" };
-                _context.Add(vendor);
-                _context.SaveChangesAsync();
+            using (var _context = new ApplicationDbContext (options)) {
+                Supplier vendor = new Supplier () { Id = "1", Name = "Samsung" };
+                _context.Add (vendor);
+                _context.SaveChangesAsync ();
 
-                var controller = new SuppliersController(_context);
-                Supplier supp = new Supplier();
+                var controller = new SuppliersController (_context);
+                Supplier supp = new Supplier ();
 
-                Task<IActionResult> result = controller.Edit(vendor.Id);
-                var editedResult = _context.Suppliers.SingleOrDefault(e => e.Id == vendor.Id);
-                if (editedResult != null)
-                {
+                Task<IActionResult> result = controller.Edit (vendor.Id);
+                var editedResult = _context.Suppliers.SingleOrDefault (e => e.Id == vendor.Id);
+                if (editedResult != null) {
 
                     editedResult.Name = "LG";
-                    _context.Update(editedResult);
-                    _context.SaveChanges();
+                    _context.Update (editedResult);
+                    _context.SaveChanges ();
                 }
 
                 //Then
-                Assert.NotNull(vendor);
-                Assert.False(editedResult.Id == "a");
-                Assert.True(editedResult.Name == "LG");
-                
+                Assert.NotNull (vendor);
+                Assert.False (editedResult.Id == "a");
+                Assert.True (editedResult.Name == "LG");
+
             }
         }
 
         [Fact]
-        public void CheckIndexSuppliers()
-        {
+        public void CheckIndexSuppliers () {
 
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "aspnet-InventorySystem")
+            var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
+                .UseInMemoryDatabase (databaseName: "aspnet-InventorySystem")
                 .Options;
-            using (var _context = new ApplicationDbContext(options))
-            {
+            using (var _context = new ApplicationDbContext (options)) {
 
-                List<Supplier> supList = new List<Supplier>() {
-                new Supplier { Id = "1", Name = "LG"},
-                new Supplier { Id = "2", Name = "Samsung"},
-                new Supplier { Id = "3", Name = "MI"},
-                
+                List<Supplier> supList = new List<Supplier> () {
+                new Supplier { Id = "1", Name = "LG" },
+                new Supplier { Id = "2", Name = "Samsung" },
+                new Supplier { Id = "3", Name = "MI" },
+
                 };
 
-                _context.AddRange(supList);
-                _context.SaveChangesAsync();
+                _context.AddRange (supList);
+                _context.SaveChangesAsync ();
 
-                var controller = new SuppliersController(_context);
-                IActionResult result = controller.Index() as IActionResult;
+                SuppliersController controller = new SuppliersController (_context);
+                IActionResult result = controller.Index () as IActionResult;
 
-                var resultEdit = _context.Suppliers.ToList();
-                /*var viewResult =*/ //Assert.IsType<ViewResult>(result);
-                //var model = Assert.IsAssignableFrom<IEnumerable<Supplier>>(viewResult.Model);
-
-
-                Assert.True(resultEdit.Count() == 3);
+                var resultEdit = _context.Suppliers.ToList ();
+                var viewResult = Assert.IsType<ViewResult> (result);
+                var model = Assert.IsAssignableFrom<IEnumerable<Supplier>> (viewResult.Model);
+                Assert.Equal (resultEdit.Count (), model.Count ());
             }
 
         }
     }
 }
-
