51f0d5fa45d9ef921e0185cddef4a0fa0c2661b1
Suli <suleiman.sultanov@gmail.com>
2019-02-13 11:25:31 +0600

    #131 test employe controller

---
+++ inventory_accounting_system/UnitTestProjectIAS/TestEventController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using inventory_accounting_system.Controllers;
+using inventory_accounting_system.Data;
+using inventory_accounting_system.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectIAS
+{
+    [TestClass]
+    public class TestEventController
+    {
+        private ApplicationDbContext context = AssetEventEdit.UseInMemoryDataBase();
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            context.Add(new Event()
+            {
+                Id = "1",
+                Title = "new"
+            });
+            var controller = new EventsController(context, null);
+
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
