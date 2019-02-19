822f5ada42ba502def5ac0755fda973b89b7e15a
jatisto <j.atisto@gmail.com>
2019-02-19 15:15:46 +0600

    #32 BagFix Delete setting and layout page report

--- inventory_accounting_system/inventory_accounting_system/Startup.cs
+++ inventory_accounting_system/inventory_accounting_system/Startup.cs
@@ -2,75 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using inventory_accounting_system.Data;
+using inventory_accounting_system.Hubs;
 using inventory_accounting_system.Interface;
 using inventory_accounting_system.Models;
 using inventory_accounting_system.Services;
-using inventory_accounting_system.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace inventory_accounting_system
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace inventory_accounting_system {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
 
-            services.AddIdentity<Employee, IdentityRole>(options => {
+            services.AddIdentity<Employee, IdentityRole> (options => {
                     options.Password.RequireDigit = false;
                     options.Password.RequireLowercase = false;
                     options.Password.RequireUppercase = false;
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequiredLength = 3;
-            })
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext> ()
+                .AddDefaultTokenProviders ();
 
-            services.AddTransient<IEmailSender, EmailSender>();
-            services.AddScoped<FileUploadService>();
-            services.AddMvc();
-            services.AddSignalR();
+            services.AddTransient<IEmailSender, EmailSender> ();
+            services.AddScoped<FileUploadService> ();
+            services.AddMvc ();
+            services.AddSignalR ();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();                
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseBrowserLink ();
+                app.UseDeveloperExceptionPage ();
+                app.UseDatabaseErrorPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles ();
 
-            app.UseAuthentication();
+            app.UseAuthentication ();
 
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChatHub>("/chatHub");
+            app.UseSignalR (routes => {
+                routes.MapHub<ChatHub> ("/chatHubs");
             });
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseMvc (routes => {
+               
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}");
             });
