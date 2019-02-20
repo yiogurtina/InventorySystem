097129278bff650294b3fc49bc2cb6535c2274e9
EugeneTM <j.atisto@gmail.com>
2019-02-21 01:40:29 +0600

    #135 page layout Offices/ ALL page finish

--- inventory_accounting_system/inventory_accounting_system/ViewModel/OfficeViewModel.cs
+++ inventory_accounting_system/inventory_accounting_system/ViewModel/OfficeViewModel.cs
@@ -1,21 +1,18 @@
-﻿using inventory_accounting_system.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using inventory_accounting_system.Models;
 
-namespace inventory_accounting_system.ViewModel
-{
-    public class OfficeIndexViewModel
-    {
+namespace inventory_accounting_system.ViewModel {
+    public class OfficeIndexViewModel {
         //public Employee Manager { get; set; }
         public IEnumerable<CategoryAssetCountViewModel> CategoryAssetCountViewModels { get; set; }
         public IEnumerable<Employee> Employees { get; set; }
         public Office Office { get; set; }
     }
 
-    public class CategoryAssetCountViewModel
-    {
+    public class CategoryAssetCountViewModel {
         public string Id { get; set; }
         public string CategoryName { get; set; }
         public int AssetsCount { get; set; }
