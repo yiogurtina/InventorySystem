049ae91d4ad228adb5a1efff04643cdf791fbb2d
EugeneTM <j.atisto@gmail.com>
2019-02-24 02:13:06 +0600

    #125 Completed the Notification of messages role Manager

---
+++ inventory_accounting_system/inventory_accounting_system/ViewComponents/OrderListViewComponent.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using inventory_accounting_system.Data;
+using inventory_accounting_system.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace inventory_accounting_system.ViewComponents {
+    public class OrderListViewComponent : ViewComponent {
+        #region Dependency Injection
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Employee> _userManager;
+
+        public OrderListViewComponent (ApplicationDbContext context, UserManager<Employee> userManager) {
+            _context = context;
+            _userManager = userManager;
+        }
+        #endregion
+
+        public IViewComponentResult Invoke () {
+
+            IQueryable<OrderEmployee> empOrders = _context.OrderEmployees
+                .Include (a => a.Asset)
+                .Include (a => a.Office)
+                .Include (a => a.EmployeeFrom)
+                .Include (a => a.EmployeeTo)
+                .Where (e => e.Status == "New");
+                
+            return View (empOrders);
+
+        }
+    }
+}
